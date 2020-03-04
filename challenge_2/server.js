0e066179d534a84cf5210a451cb8ea089d72b86a
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-03-03 16:52:59 -0800

    Generate CSV report and send as response.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,10 +2,51 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const bodyParser = require('body-parser')
+const fs = require('fs')
 
 app.use('/', express.static(path.join(__dirname, './client')))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/report', (req, res) => {
+    res.send('Getting');
+    
+})
+
+app.post('/api/report', (req, res) => {
+    const data = JSON.parse(req.body.json_file);
+
+    ///////////Generate CSV report//////////////
+    const generateReport = data => {
+        let report = '';
+        let columnTitles = Object.keys(data);
+        columnTitles.pop()
+        let childrenValues = [];
+        
+        const findChildren = data => {
+            var values = Object.values(data);
+            values.pop();
+            childrenValues.push(values); 
+
+            if (data.children) {
+                for (var i = 0; i < data.children.length; i++)
+                findChildren(data.children[i]);
+            }
+        } 
+        findChildren(data);  
+        childrenValues = childrenValues.join('\n');
+        columnTitles = columnTitles.join();
+        report = columnTitles + '\n' + childrenValues;
+        console.log(report)    
+        return report;
+
+    }
+let generatedCSV = generateReport(data);
+res.send(generatedCSV);
+})
+  
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 
